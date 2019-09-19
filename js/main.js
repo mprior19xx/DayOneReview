d0ea6f18c77cc5b9402db7a5558a51029c492fc0
Mike Prior <43096449+mprior19xx@users.noreply.github.com>
2019-09-19 13:12:44 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
 //allways import files first
 //takes everything from DataObject and labels as "Person"
-import Person from './module/DataObject.js';
+import Team from './module/DataObject.js';
 
 //this is an IFE -> immediately invoked function expression
 //this is the Javascript Module Pattern
 (() => {
     console.log('fired!');
 
-    console.log(Person);
+    let bioData = document.querySelector('.bio-wrapper').children;
+
+    // populate the children with the data object values
+    // bioData[0].src = "images/" + Person.avatar;
+    // bioData[1].textContent = Person.name;
+    // bioData[2].textContent = Person.role;
+    // bioData[3].textContent = Person.bio;
+
+    function parsePersonData(){
+        //this. = the element that causes this function (the button you click on)
+        let person = this.textContent;
+
+        bioData[0].src = `images/${Team[person].avatar}`;
+        bioData[1].textContent = Team[person].name;
+        bioData[2].textContent = Team[person].role;
+        bioData[3].textContent = Team[person].bio;
+    }
+    
+    // loop thru then build some UI
+    for (let person in Team) {
+        console.log(person);
+    
+
+    //create a button for each member
+    let currentButton = document.createElement('button');
+    //add persons name to button
+    currentButton.textContent = person;
+    //add event handler to the bottom
+    currentButton.addEventListener('click', parsePersonData);
+    //put the button in team section html
+    document.querySelector('.team').appendChild(currentButton);
+        }
 })();
--- js/module/DataObject.js
+++ js/module/DataObject.js
@@ -1,8 +1,38 @@
 //Set up object ("default") to be exported
 export default {
-    name: "Mike",
-    role: "Designer",
-    height: "6'3",
-    age: "27",
-    hair: "brown",
+    Mike : {
+        name: "Mike",
+        role: "Designer",
+        bio: "you miss 100% of the shots you dont take -michael jordan -michael scott -michael prior",
+        avatar: "olaf.jpeg"
+    },
+    
+    Justin : {
+        name: "Justin",
+        role: "Coordinator",
+        bio: "im the boss, bitch",
+        avatar: "justin.jpeg"
+    },
+
+    Joe : {
+        name: "Joe",
+        role: "Professor",
+        bio: "bring a sketchbook",
+        avatar: "pooh.jpeg"
+    },
+
+    Pan : {
+        name: "Pan",
+        role: "Professor",
+        bio: "table table table table",
+        avatar: "pan.jpg"
+    },
+
+    Trevor : {
+        name: "Trevor",
+        role: "Professor",
+        bio: "javajavajavajavajavaGITHUB",
+        avatar: "zoolander.jpg"
+    }
 }
+
